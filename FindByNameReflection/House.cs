6073eae6ba6b7dd16d6f2b9f7922af0548b4017a
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-25 23:45:37 +0200

    Add files via upload

---
+++ FindByNameReflection/House.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindByNameReflection
+{
+    internal class House
+    {
+        public int Age;
+        public int Size;
+        public string Street;
+        public House(int age, int size, string street)
+        {
+            Age = age;
+            Size = size;
+            Street = street;
+        }
+        public override string ToString()
+        {
+            return $"\nAge:\t{Age}\nSize:\t{Size}\nStreet:\t{Street}";
+        }
+    }
+}
---
+++ FindByNameReflection/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FindByNameReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House h1 = new House(10, 100, "Philadelphia");
+            int area = (int) typeof(House).GetField("Size").GetValue(h1);
+            typeof(House).GetField("Street").SetValue(h1, "Lincoln");
+            Console.WriteLine(area);
+            Console.WriteLine(h1.Street);
+        }
+    }
+}
